741306c1ef770b54a75d0f35369069e4dcdb1850
Mac <mac@Macs-MacBook-Pro.local>
2023-08-07 16:04:43 +0100

    create tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Import wait_random from 0-basic_async_syntax.
+    Write a function (do not create an async function, use the regular function
+    syntax to do this)"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ Tasks """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
