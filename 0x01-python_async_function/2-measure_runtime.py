20ac6f280a70a448c2c5d6f093982471f9c8a03d
Mac <mac@Macs-MacBook-Pro.local>
2023-08-07 16:00:34 +0100

    measure runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" imports wait_n into 2-measure_runtime.py.
+    Create a measure_time function with integers n and max_delay as arguments
+    that measures the total execution time """
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """ Measure the runtime """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
